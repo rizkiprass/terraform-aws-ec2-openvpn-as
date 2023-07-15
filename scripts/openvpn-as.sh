1873e106ef9a3232f89f0bc5f50990e1b8e240fd
rizkiprass <rizkiprass@gmail.com>
2023-07-15 10:39:45 +0700

    add script add user openvpn

--- scripts/openvpn-as.sh
+++ scripts/openvpn-as.sh
@@ -4,3 +4,19 @@ apt update && apt -y install ca-certificates wget net-tools gnupg
 wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
 echo "deb http://as-repository.openvpn.net/as/debian focal main">/etc/apt/sources.list.d/openvpn-as-repo.list
 apt update && apt -y install openvpn-as | grep -oP 'To login please use the "openvpn" account with "[^"]+" password.' > /home/ubuntu/login-user-pass.txt
+
+#configuration user
+cd /usr/local/openvpn_as/scripts/
+
+#Set the default authentication mode to local:
+./sacli --key "auth.module.type" --value "local" ConfigPut
+./sacli start
+
+#Add a new user from scratch:
+./sacli --user ${user_openvpn} --key "type" --value "user_connect" UserPropPut
+
+# Generate a random password
+password=$(openssl rand -base64 12)
+
+# Set password for a user
+./sacli --user ${user_openvpn} --new_pass "$password" SetLocalPassword
